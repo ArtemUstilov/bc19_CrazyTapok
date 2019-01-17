1e11740a272e30f6dc1c15861772608a830ddcd0
Artem <artem.ustilov@gmail.com>
2019-01-17 11:56:29 +0200

    rewrite to functional

--- blue/distance.js
+++ blue/distance.js
@@ -37,27 +37,24 @@ function incrementNeighbors(mapSheme, map, wave, pos){
     let nextWave = [];
     if(!wave || !wave.length)return;
     let value = mapSheme[wave[0].x][wave[0].y] + 1;
-    for(let i = 0; i < wave.length; i++){
-        let neighbors = getNearbyCells(
-            wave[i], (x)=>
+    wave.forEach(waveI=>{
+        getNearbyCells(
+            waveI, (x)=>
                 x.insideSquare(map.length)&& map[x.y][x.x]&& !mapSheme[x.x][x.y]&& !x.equal(pos)
-        )
-        for(let j = 0; j < neighbors.length; j++){
-            mapSheme[neighbors[j].x][neighbors[j].y] = value;
-            nextWave.push(neighbors[j]);
-        }
-    }
+        ).forEach(n=>{
+            mapSheme[n.x][n.y] = value;
+            nextWave.push(n);
+            })
+    })
     return nextWave;
 }
 function getNearbyCells(p,validCB){
     //gets nearby cells
-    let t = [p.shift(-1,0), p.shift(1,0), p.shift(0, -1), p.shift(0,1)]
+    return [p.shift(-1,0), p.shift(1,0), p.shift(0, -1), p.shift(0,1)]
         .filter(x=>validCB(x));
-    return t;
 }
 function decrementedCell(mapSheme, lastStep) {
     //finds cell that has n-1 value to find all path to pos
-    let t = getNearbyCells(lastStep, (x)=>x.insideSquare(mapSheme.length))
-    let f = t.find((cell)=>mapSheme[cell.x][cell.y] == mapSheme[lastStep.x][lastStep.y] - 1)
-    return f;
+    return getNearbyCells(lastStep, (x)=>x.insideSquare(mapSheme.length))
+        .find((cell)=>mapSheme[cell.x][cell.y] == mapSheme[lastStep.x][lastStep.y] - 1)
 }
