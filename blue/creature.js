b5fd04ae7bb03be95be36120fb75b56bc2806972
BatyaIsHere <rshchibrik@gmail.com>
2019-01-17 17:28:55 +0200

    Mininig

--- blue/creature.js
+++ blue/creature.js
@@ -7,40 +7,27 @@ export default class Creature {
         this.log = _this.log.bind(_this);
         this.updatePosition();
         this.castles = [];
-        this.makeHomeCordinates();
         this.position;
-        this.home;
+
         this.width = this.robot.map.length;
     }
     updatePosition(){
         this.position = new Point(this.robot.me.x, this.robot.me.y);
     }
-    makeHomeCordinates(){
-        if(this.robot.me.unit == SPECS.CASTLE){
-            this.home = new Point(this.position.x, this.position.y);
-            return;
-        }
-        for(let i = Math.max(0, this.position.x - 1); i <= Math.min(this.width,this.position.x + 1); i++){
-            for(let j = Math.max(0,this.position.y - 1); j <= Math.min(this.width,this.position.y + 1); j++) {
-                let robot = this.robot.getVisibleRobotMap()[j][i];
-                if (robot > 0) {
-                    robot = this.robot.getRobot(robot);
-                    if (robot.unit == SPECS.CASTLE || robot.unit == SPECS.CHURCH) {
-                        let p = new Point(robot.x, robot.y);
-                        if(robot.unit == SPECS.CASTLE)
-                            this.castles.push(p);
-                        this.home = p;
-                        return;
-                    }
-                }
-            }
-        }
-    }
-    findFreePlace() {
+
+    findFreePlace(dest) {
         let freeP = [];
         let robotsMap = this.robot.getVisibleRobotMap();
-        for (let i = Math.max(0, this.position.x - 1); i <= Math.min(this.width, this.position.x + 1); i++) {
-            for (let j = Math.max(0, this.position.y - 1); j <= Math.min(this.width, this.position.y + 1); j++) {
+        let x, y;
+        if(dest){
+            x = dest.x;
+            y = dest.y;
+        }else{
+           x = this.position.x;
+           y = this.position.y;
+        }
+        for (let i = Math.max(0, x - 1); i <= Math.min(this.width, x + 1); i++) {
+            for (let j = Math.max(0, y - 1); j <= Math.min(this.width, y + 1); j++) {
                 if (!robotsMap[j][i] && this.robot.map[j][i])
                     freeP.push(new Point(i, j));
             }
--- blue/piligrim.js
+++ blue/piligrim.js
@@ -2,29 +2,62 @@ import Point from './point.js';
 import findClosestResource from './mapScan.js';
 import WalkingRobot from 'walkingRobot.js';
 
-export default class Piligrim extends WalkingRobot{
+export default class Piligrim extends WalkingRobot {
     constructor(_this) {
         super(_this);
-        this._this = _this;
-        this.readyToMine = false;
+        this.actionType = 0;
+        // 0-goMine, 1-readyToMine, 2-goHome, 3-readyToGive
     }
+
     removePath() {
-        this.currentPath = [];
-        this.destination = undefined;
-        this.isGoing = false;
-        this.readyToMine = true;
+        super.removePath();
+        this.actionType = 1;
+        if (this.isFull(100,100))
+            this.actionType = 3;
     }
+
+    isFull(fuelNum, karboniteNum) {
+        return this.robot.me.fuel == fuelNum || this.robot.me.karbonite == karboniteNum;
+    }
+
     goMine(ignore) {
         let point = new Point(...Object.values(findClosestResource(this.position.x, this.position.y, this.robot.fuel_map, [])));
-        this.updatePath(point);
+        this.updatePath(point, this.robot.getVisibleRobotMap());
+        return this.step();
     }
+
+    goHome() {
+        if (this.position.distanceSq(this.home) > 2)
+            this.updatePath(this.findFreePlace(this.home)[0]);
+        return this.step();
+    }
+
     do_someth(ign) {
         this.updatePosition();
-        if(!this.isGoing && !this.readyToMine)this.goMine(ign);
-        if (this.readyToMine) {
+
+        this.log(this.actionType);
+        switch (this.actionType) {
+            case 0:
+                return this.goMine(ign);
+            case 1:
+                if (this.isFull(90,100))
+                    this.actionType = 2;
+                return this.robot.mine();
+            case 2:
+                return this.goHome();
+            case 3:
+                this.actionType = 0;
+                return this.robot.give(...this.position.deltaArray(this.home), this.robot.me.karbonite, this.robot.me.fuel);
+        }
+        if (!this.isGoing && !this.readyToMine) this.goMine(ign);
+        if (this.readyToMine && this.robot.me.fuel < 91) {
             this.log(this.position.x)
             return this.robot.mine()
         } else if (this.isGoing) {
+            return this.step();
+        } else {
+            this.log(this.home + "  " + this.findFreePlace(this.home).toString());
+            this.updatePath(this.findFreePlace(this.home)[0]);
             return this.step();
         }
     }
